98a463f2adbba19a96379628999daf8a2a0a5c24
FilipKnj <filip23.soa@gmail.com>
2023-02-08 16:37:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const url = `https://meme-api.com/gimme`;
+const img = document.querySelector('img');
+const btn = document.querySelector('button');
+const title = document.querySelector('h3');
+
+
+const getMeme = () => {
+    fetch(url).then(res => res.json()).then(data => {
+        img.setAttribute('src', `${data.preview[3]}`);
+        title.innerText = `${data.title}`;
+    })
+}
+
+btn.addEventListener('click',getMeme);
+window.addEventListener('load',getMeme);
